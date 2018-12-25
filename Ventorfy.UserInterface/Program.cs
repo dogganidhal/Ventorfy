4c69c36daa21873f2f00a381adbab4b09ce99dca
Dogga Nidhal <dogga.nidhal@gmail.com>
2018-12-25 17:17:50 +0000

    Convert to .Net Framwork 4.6.1

--- Ventorfy.UserInterface/Program.cs
+++ Ventorfy.UserInterface/Program.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ventorfy.DataAccess.Repository;
+using Ventorfy.DataAccess.Repository.Users;
 
 namespace Ventorfy.UserInterface
 {
 	static class Program
 	{
+
+		private static readonly IUserRepository _UserRepository = RepositoryFactory.Instance.UserRepository;
+
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
